86d9db918df4e1f9cda102ba42eb512641daab5f
s20020 <s20020@std.it-college.ac.jp>
2021-12-08 10:39:33 +0900

    binding完了。

--- app/src/main/java/jp/ac/it_college/std/s20020/calendar_u/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/calendar_u/MainActivity.kt
@@ -2,10 +2,14 @@ package jp.ac.it_college.std.s20020.calendar_u
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import jp.ac.it_college.std.s20020.calendar_u.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
