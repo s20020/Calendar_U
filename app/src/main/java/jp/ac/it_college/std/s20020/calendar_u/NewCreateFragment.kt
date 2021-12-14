6cec6873bf0c963592b1df85cd67f7f8ddb6f0a2
s20010 <s20010@std.it-college.ac.jp>
2021-12-14 11:37:39 +0900

    12/14 11:37

--- app/src/main/java/jp/ac/it_college/std/s20020/calendar_u/NewCreateFragment.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/calendar_u/NewCreateFragment.kt
@@ -1,5 +1,6 @@
 package jp.ac.it_college.std.s20020.calendar_u
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.os.Bundle
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
@@ -22,6 +24,7 @@ class NewCreateFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_create, container, false)
     }
+
 }
 
 
