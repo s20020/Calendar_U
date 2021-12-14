2ab26797d5ee74569f99ad844917c61765d240da
s20010 <s20010@std.it-college.ac.jp>
2021-12-14 10:32:29 +0900

    stuck in the middle

---
+++ app/src/main/java/jp/ac/it_college/std/s20020/calendar_u/NewCreateFragment.kt
@@ -0,0 +1,27 @@
+package jp.ac.it_college.std.s20020.calendar_u
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+
+class NewCreateFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_create, container, false)
+    }
+}
+
+
